10e77e3b07c9c328f5dc2db5c51873a4b38204f7
herbertmaiamtt <herbertmaiamtt@gmail.com>
2026-01-12 14:25:58 -0300

    feat: implement loadConfigurationFile function on ConfigurationManager class

--- src/main/java/com/httpserver/config/ConfigurationManager.java
+++ src/main/java/com/httpserver/config/ConfigurationManager.java
@@ -1,8 +1,15 @@
 package com.httpserver.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.httpserver.util.Json;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ConfigurationManager {
 
-    private static ConfigurationManager myCongigurationManager;
+    private static ConfigurationManager myConfigurationManager;
     private static Configuration myCurrentConfiguratuion;
 
     private ConfigurationManager(){
@@ -10,15 +17,22 @@ public class ConfigurationManager {
     }
 
     public static ConfigurationManager getInstance(){
-        if(myCongigurationManager == null)
-            myCongigurationManager = new ConfigurationManager();
+        if(myConfigurationManager == null)
+            myConfigurationManager = new ConfigurationManager();
 
-        return myCongigurationManager;
+        return myConfigurationManager;
     }
 
     // Used to load a configuration file by the path provided
-    public void loadConfigurationFile(String filePath){
-
+    public void loadConfigurationFile(String filePath) throws IOException {
+        FileReader fileReader = new FileReader(filePath);
+        StringBuffer sb = new StringBuffer();
+        int i;
+        while((i = fileReader.read()) != -1){
+            sb.append((char)i);
+        }
+        JsonNode conf = Json.parse(sb.toString());
+        myCurrentConfiguratuion = Json.fromJson(conf, Configuration.class);
     }
 
     // Returns the Current loaded Configuration
