5a65154bb37a2d9e7d4c7b6fad434b0db8591657
herbertmaiamtt <herbertmaiamtt@gmail.com>
2026-01-12 12:10:59 -0300

    feat: init maven project and create HttpServer class

---
+++ src/main/java/com/httpserver/HttpServer.java
@@ -0,0 +1,16 @@
+package com.httpserver;
+
+/*
+*
+* Driver Class for the Http Server
+*
+*/
+public class HttpServer {
+
+    public static void main(String[] args){
+
+        System.out.println("Server starting");
+
+    }
+
+}
