f654d32da22e50c688e6e902eb70fcf4c34e1164
herbertmaiamtt <herbertmaiamtt@gmail.com>
2026-01-14 16:18:58 -0300

    test: verify port and webroot parsing from json

--- src/main/java/com/httpserver/HttpServer.java
+++ src/main/java/com/httpserver/HttpServer.java
@@ -1,5 +1,8 @@
 package com.httpserver;
 
+import com.httpserver.config.Configuration;
+import com.httpserver.config.ConfigurationManager;
+
 /*
 *
 * Driver Class for the Http Server
@@ -10,6 +13,12 @@ public class HttpServer {
     public static void main(String[] args){
 
         System.out.println("Server starting");
+
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+        Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
+
+        System.out.println("Using port: " + conf.getPort());
+        System.out.println("Using WebRoot: " + conf.getWebroot());
 
     }
 
