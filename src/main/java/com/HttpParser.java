a0ab9c45a60713b5613be15c2b4d8c3f40d4db99
herbertmaiamtt <herbertmaiamtt@gmail.com>
2026-01-16 16:40:05 -0300

    feat: setup HttpParser class structure and test suite

---
+++ src/main/java/com/HttpParser.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+
+public class HttpParser {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpParser.class);
+
+    public void parseHttpRequest(InputStream input){
+
+    }
+
+}
---
+++ src/test/java/com/HttpParserTest.java
@@ -0,0 +1,56 @@
+package com;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class HttpParserTest {
+
+    private HttpParser httpParser;
+
+    @BeforeAll
+    public void beforeClass(){
+        httpParser = new HttpParser();
+    }
+
+    @Test
+    void parseHttpRequest() {
+        httpParser.parseHttpRequest(
+                generateValidTestCase()
+        );
+    }
+
+    private InputStream generateValidTestCase(){
+        String rawData = "GET / HTTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:146.0) Gecko/20100101 Firefox/146.0\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n" +
+                "Accept-Language: en-US,en;q=0.5\r\n" +
+                "Accept-Encoding: gzip, deflate, br, zstd\r\n" +
+                "Sec-GPC: 1\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Cookie: JSESSIONID=9ED3505AB3AE1ADCA1AE7449B0EA15C8\r\n" +
+                "Upgrade-Insecure-Requests: 1\r\n" +
+                "Sec-Fetch-Dest: document\r\n" +
+                "Sec-Fetch-Mode: navigate\r\n" +
+                "Sec-Fetch-Site: none\r\n" +
+                "Priority: u=0, i\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(
+                rawData.getBytes(
+                        StandardCharsets.US_ASCII
+                )
+        );
+
+        return inputStream;
+    }
+
+}
